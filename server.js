d06f88cfb5d20445c70447e561565a495199fa56
coleparrish9 <128101109+coleparrish9@users.noreply.github.com>
2023-10-31 15:31:18 -0700

    gonna try this

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 
 const hbs = exphbs.create({ helpers });
@@ -37,7 +37,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pub')));
 
 app.use(routes);
 
